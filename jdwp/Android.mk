2d5b22d49cac12d5387c36c090230e2c1f1bc7d0
Elliott Hughes <enh@google.com>
2012-02-27 11:39:02 -0800

    Add support for running the JDWP tests on RI.
    Turns out that there are a bunch of VM-crashing bugs in the RI. We currently
    have no native crashes.
    
    Change-Id: I7e157fae4c4e8cc1733b0b840dc2f64b12eb0ed6

--- jdwp/Android.mk
+++ jdwp/Android.mk
@@ -22,6 +22,13 @@ LOCAL_NO_EMMA_COMPILE := true
 LOCAL_MODULE_PATH := $(TARGET_OUT_DATA)
 include $(BUILD_JAVA_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(harmony_jdwp_test_src_files)
+LOCAL_JAVA_LIBRARIES := junit
+LOCAL_MODULE_TAGS := tests
+LOCAL_MODULE := $(local_module_name)-host
+include $(BUILD_HOST_JAVA_LIBRARY)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 jdwp_test_runtime := oatexecd
@@ -35,8 +42,14 @@ run-jdwp-tests: $(TARGET_OUT_DATA)/$(jdwp_tests_jar) $(TARGET_OUT_JAVA_LIBRARIES
 	adb sync
 	adb shell $(jdwp_test_runtime) -cp $(jdwp_test_classpath) \
           -Djpda.settings.verbose=true \
-          -Djpda.settings.syncHost=localhost \
           -Djpda.settings.syncPort=34016 \
           -Djpda.settings.debuggeeJavaPath="$(jdwp_test_runtime)" \
-          -Djpda.settings.debuggeeClassPath="$(jdwp_test_classpath)" \
+          org.apache.harmony.jpda.tests.share.AllTests
+
+.PHONY: run-jdwp-tests-ri
+run-jdwp-tests-ri: $(HOST_OUT_JAVA_LIBRARIES)/$(local_module_name)-host.jar $(HOST_OUT_JAVA_LIBRARIES)/junit.jar
+	java -cp $(HOST_OUT_JAVA_LIBRARIES)/$(local_module_name)-host.jar:$(HOST_OUT_JAVA_LIBRARIES)/junit.jar \
+          -Djpda.settings.verbose=true \
+          -Djpda.settings.syncPort=34016 \
+          -Djpda.settings.debuggeeJavaPath=java \
           org.apache.harmony.jpda.tests.share.AllTests
