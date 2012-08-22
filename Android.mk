05aba2478427cb86445e79316943a038e536a224
Ying Wang <wangying@google.com>
2012-08-21 17:27:19 -0700

    Remove obsolete variables.
    Bug: 6987838
    Now emma is only enabled for apks.
    
    Change-Id: I358f32d7b77283a1dbca1da16a94b291c35fd079

--- Android.mk
+++ Android.mk
@@ -54,8 +54,6 @@ LOCAL_JAVA_LIBRARIES := core core-junit
 LOCAL_JAVACFLAGS := $(harmony_test_javac_flags)
 LOCAL_MODULE_TAGS := tests
 LOCAL_MODULE := apache-harmony-tests
-LOCAL_NO_EMMA_INSTRUMENT := true
-LOCAL_NO_EMMA_COMPILE := true
 include $(BUILD_STATIC_JAVA_LIBRARY)
 
 ifeq ($(WITH_HOST_DALVIK),true)
@@ -67,8 +65,6 @@ ifeq ($(WITH_HOST_DALVIK),true)
     LOCAL_JAVACFLAGS := $(harmony_test_javac_flags)
     LOCAL_MODULE_TAGS := tests
     LOCAL_MODULE := apache-harmony-tests-hostdex
-    LOCAL_NO_EMMA_INSTRUMENT := true
-    LOCAL_NO_EMMA_COMPILE := true
     LOCAL_BUILD_HOST_DEX := true
     include $(BUILD_HOST_JAVA_LIBRARY)
 endif
