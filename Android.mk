db08d5168e51be52dd50e3bb9015fe69f106fb36
Ying Wang <wangying@google.com>
2012-08-23 11:21:13 -0700

    Fix build warning.
    Change-Id: I27d3066053a6fb45485f37d57c066375714cc3c1

--- Android.mk
+++ Android.mk
@@ -63,7 +63,6 @@ ifeq ($(WITH_HOST_DALVIK),true)
     LOCAL_NO_STANDARD_LIBRARIES := true
     LOCAL_JAVA_LIBRARIES := core-hostdex core-junit-hostdex
     LOCAL_JAVACFLAGS := $(harmony_test_javac_flags)
-    LOCAL_MODULE_TAGS := tests
     LOCAL_MODULE := apache-harmony-tests-hostdex
     LOCAL_BUILD_HOST_DEX := true
     include $(BUILD_HOST_JAVA_LIBRARY)
